3f76e4b3eb8495c9f790a467d791c2d83fd5beaf
Jan-Grimo Sobez <sobezj@student.ethz.ch>
2016-09-30 09:53:33 +0200

    almost completely working for multidentate ligands
    two test cases still fail, must look into it

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all
+.PHONY: all 
 all:
-	g++ -std=c++14 -otest -L/usr/lib/x86_64-linux-gnu/ tests.cpp -lboost_unit_test_framework
+	g++ -O2 -std=c++14 -otest -L/usr/lib/x86_64-linux-gnu/ tests.cpp -lboost_unit_test_framework
 	./test --report_level=detailed
--- tests.cpp
+++ tests.cpp
@@ -10,6 +10,12 @@
 
 #include "algorithm.hpp"
 
+/* TODO
+ * - investigate failures!
+ * - Issues hindering generalizability
+ *   Custom rotation algorithm with the symmetry operations
+ */
+
 template<typename T>
 std::ostream& operator << (std::ostream& os, const std::vector<T>& vector) {
     for(unsigned i = 0; i < vector.size(); i++) {
@@ -23,11 +29,16 @@ std::ostream& operator << (std::ostream& os, const std::vector<T>& vector) {
 }
 
 void run_tests(
-    const std::vector<Assignment>& test_cases,
+    const std::vector<
+        std::vector<char>
+    >& test_cases,
     const std::vector<unsigned>& expected
 ) {
+    assert(test_cases.size() == expected.size());
     for(unsigned i = 0; i < test_cases.size(); i++) {
-        auto unique = unique_assignments(test_cases[i]);
+        auto unique = unique_assignments(
+            Assignment(test_cases[i])
+        );
         BOOST_CHECK ( unique.size() == expected[i] );
         if(unique.size() != expected[i]) {
             std::cout << "Mismatch: Expected " << expected[i]
@@ -40,6 +51,176 @@ void run_tests(
         }
     }
 }
+
+void run_test(
+    const Assignment& instance,
+    const unsigned& expected
+) {
+    auto unique = unique_assignments(
+        instance
+    );
+    BOOST_CHECK ( unique.size() == expected );
+    if(unique.size() != expected) {
+        std::cout << "Mismatch: Expected " << expected
+            << " assignments for: " << std::endl;
+        std::cout << instance << std::endl;
+        std::cout << ", got " << unique.size() << std::endl;
+        std::cout << "Unique assignments: " << std::endl;
+        for(const auto& assignment: unique) {
+            std::cout << assignment << std::endl;
+        }
+    }
+}
+
+
+BOOST_AUTO_TEST_CASE( assignment_column ) {
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            {true, false, true}
+        ) == AssignmentColumn(
+            'A',
+            {true, false, true}
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            {true, false, true}
+        ) != AssignmentColumn(
+            'A',
+            {false, false, true}
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            {true, false, true}
+        ) != AssignmentColumn(
+            'B',
+            {true, false, true}
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            {true, false, true}
+        ) < AssignmentColumn(
+            'B',
+            {true, false, true}
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            {true, false, false}
+        ) < AssignmentColumn(
+            'A',
+            {true, false, true}
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            {false, false, true}
+        ) < AssignmentColumn(
+            'A',
+            {true, false, false}
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            std::vector<bool>()
+        ) == AssignmentColumn(
+            'A',
+            std::vector<bool>()
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            std::vector<bool>()
+        ) < AssignmentColumn(
+            'B',
+            std::vector<bool>()
+        )
+    );
+
+    BOOST_CHECK(
+        AssignmentColumn(
+            'A',
+            {true}
+        ) < AssignmentColumn(
+            'B',
+            {false}
+        )
+    );
+    BOOST_CHECK(
+        (
+            AssignmentColumn(
+                'B',
+                {false}
+            ) < AssignmentColumn(
+                'A',
+                {true}
+            )
+        ) == false
+    );
+}
+
+BOOST_AUTO_TEST_CASE( assignment_basics ) {
+    // Constructors
+    Assignment instance(
+        {'A', 'A', 'A', 'A', 'A', 'A'}
+    );
+    Assignment instance_with_bonded_ligands(
+        {'A', 'A', 'A', 'A', 'A', 'A'},
+        {
+            {true, true, false, false, false, false},
+            {false, false, true, true, false, false},
+            {false, false, false, false, true, true}
+        }
+    );
+    Assignment instance_with_ordered_ligands(
+        {'A', 'A', 'A', 'A', 'A', 'A'},
+        {
+            {false, false, false, false, true, true},
+            {false, false, true, true, false, false},
+            {true, true, false, false, false, false}
+        }
+    );
+
+    // all constructs sort the ligands
+    BOOST_CHECK(
+        instance.ligand_connections_are_ordered() == true
+    );
+    BOOST_CHECK(
+        instance_with_bonded_ligands.ligand_connections_are_ordered() == true
+    );
+    BOOST_CHECK(
+        instance_with_ordered_ligands.ligand_connections_are_ordered() == true
+    );
+
+    BOOST_CHECK(
+        instance_with_bonded_ligands == instance_with_ordered_ligands
+    );
+
+    BOOST_CHECK(
+        instance_with_bonded_ligands.is_rotationally_superimposable(
+            instance_with_ordered_ligands
+        )
+    );
+}
+
+// all cases
 
 BOOST_AUTO_TEST_CASE( M_A ) {
     run_tests(
@@ -119,5 +300,135 @@ BOOST_AUTO_TEST_CASE( M_ABCDEF ) {
         }, {
             30
         }
+    );
+}
+
+// and (some) multidentate cases
+
+BOOST_AUTO_TEST_CASE( M_AA3 ) {
+    run_test(
+        Assignment(
+            {'A', 'A', 'A', 'A', 'A', 'A'},
+            {
+                {true, true, false, false, false, false},
+                {false, false, true, true, false, false},
+                {false, false, false, false, true, true}
+            }
+        ),
+        2
+        /* besides the two cis-cis-cis enantiomers, there are
+         * cis-cis-trans and trans-trans-trans isomers, these are
+         * removed by default!
+         */
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_AB3 ) {
+    run_test(
+        Assignment(
+            {'A', 'B', 'A', 'B', 'A', 'B'},
+            {
+                {true, true, false, false, false, false},
+                {false, false, true, true, false, false},
+                {false, false, false, false, true, true}
+            }
+        ),
+        4
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_AB2_C_D ) {
+    run_test(
+        Assignment(
+            {'A', 'B', 'A', 'B', 'C', 'D'},
+            {
+                {true, true, false, false, false, false},
+                {false, false, true, true, false, false}
+            }
+        ),
+        11
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_AA_BC_D_E ) {
+    run_test(
+        Assignment(
+            {'A', 'A', 'B', 'C', 'D', 'E'},
+            {
+                {true, true, false, false, false, false},
+                {false, false, true, true, false, false}
+            }
+        ),
+        10
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_AB_CD_E_F ) {
+    run_test(
+        Assignment(
+            {'A', 'B', 'C', 'D', 'E', 'F'},
+            {
+                {true, true, false, false, false, false},
+                {false, false, true, true, false, false}
+            }
+        ),
+        20
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_ABA_C_D_E ) {
+    run_test(
+        Assignment(
+            {'A', 'B', 'A', 'C', 'D', 'E'},
+            {
+                {true, true, false, false, false, false},
+                {false, true, true, false, false, false}
+            }
+        ),
+        9
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_ABC2 ) {
+    run_test(
+        Assignment(
+            {'A', 'B', 'C', 'A', 'B', 'C'},
+            {
+                {true, true, false, false, false, false},
+                {false, true, true, false, false, false},
+                {false, false, false, true, true, false},
+                {false, false, false, false, true, true}
+            }
+        ),
+        11 // fails, get 9
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_ABBA_C_D ) {
+    run_test(
+        Assignment(
+            {'A', 'B', 'B', 'A', 'C', 'D'},
+            {
+                {true, true, false, false, false, false},
+                {false, true, true, false, false, false},
+                {false, false, true, true, false, false}
+            }
+        ),
+        7
+    );
+}
+
+BOOST_AUTO_TEST_CASE( M_ABCBA_D ) {
+    run_test(
+        Assignment(
+            {'A', 'B', 'C', 'B', 'A', 'D'},
+            {
+                {true, true, false, false, false, false},
+                {false, true, true, false, false, false},
+                {false, false, true, true, false, false},
+                {false, false, false, true, true, false}
+            }
+        ),
+        7 // fails, get 5
     );
 }
