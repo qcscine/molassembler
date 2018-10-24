ae2f5b6035d6cb122e35407ac926174aa489a29b
Jan-Grimo Sobez <sobezj@student.ethz.ch>
2018-10-24 18:37:41 +0200

    CMake: Clean up configure-time output

--- src/cmake/Dependencies.cmake
+++ src/cmake/Dependencies.cmake
@@ -1,5 +1,8 @@
 find_package(Eigen3 3.1.1 REQUIRED)
 find_package(dlib 19.4 REQUIRED)
-
 set(Boost_USE_STATIC_LIBS ON)
 set(Boost_USE_MULTITHREADED ON)
+
+find_package(Boost
+  COMPONENTS regex unit_test_framework program_options filesystem system
+)
