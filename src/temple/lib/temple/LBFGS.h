6f677e042a96c5161403ea125a9817574c8f0234
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2019-05-28 16:42:22 +0200

    Temple: Further refactor LBFGS
    Temple: Further refactor LBFGS
    - Extract step initialization and acceptance
    - Extract backtracking

--- src/temple/lib/temple/LBFGS.h
+++ src/temple/lib/temple/LBFGS.h
@@ -43,8 +43,13 @@ struct EigenUpdateBuffer {
   EigenType current;
 
   void propagate() {
-    old = std::move(current);
-    current.resize(old.size());
+    /* Swapping instead of moving current into old is three moves instead of
+     * one, but avoids allocation of new memory (after moving from current, we
+     * need to resize current to have the same size). Assuming moves are
+     * cheap (trade ownership of allocated memory) and allocation with context
+     * switches could be costly, this might be cheaper.
+     */
+    std::swap(old, current);
   }
 
   void revert() {
@@ -69,6 +74,137 @@ public:
   using MatrixType = Eigen::Matrix<FloatType, Eigen::Dynamic, Eigen::Dynamic>;
   using VectorType = Eigen::Matrix<FloatType, Eigen::Dynamic, 1>;
 
+  struct StepValues {
+    detail::EigenUpdateBuffer<VectorType> parameters;
+    detail::EigenUpdateBuffer<FloatType> values;
+    detail::EigenUpdateBuffer<VectorType> gradients;
+
+    /**
+     * @brief Initialize class state and generate first direction vector
+     *
+     * @param function objective function generating values and gradients from parameters
+     * @param initialParameters Parameters passed to optimization method
+     * @param multiplier Initial step length
+     *
+     * @return First direction vector
+     */
+    template<typename UpdateFunction>
+    VectorType generateInitialDirection(
+      UpdateFunction&& function,
+      Eigen::Ref<VectorType> initialParameters,
+      FloatType multiplier
+    ) {
+      const unsigned P = initialParameters.size();
+      parameters.old = std::move(initialParameters);
+      gradients.old.resize(P);
+      gradients.current.resize(P);
+
+      // Initialize old values and gradients
+      function(parameters.old, values.old, gradients.old);
+
+      // Initialize new with a small steepest descent step
+      VectorType direction = FloatType {-0.1} * gradients.old;
+      // Create new parameters
+      parameters.current.noalias() = parameters.old + multiplier * direction;
+      // Re-evaluate the function, assigning current value and gradient
+      function(parameters.current, values.current, gradients.current);
+
+      return direction;
+    }
+
+    template<typename UpdateFunction>
+    void propagate(
+      UpdateFunction&& function,
+      const FloatType multiplier,
+      const VectorType& direction
+    ) {
+      parameters.propagate();
+      values.propagate();
+      gradients.propagate();
+
+      parameters.current.noalias() = parameters.old + multiplier * direction;
+      function(parameters.current, values.current, gradients.current);
+    }
+  };
+
+  /**
+   * @brief Adjusts @p stepLength member and ensures the current parameter
+   *   adjustment lowers the function value.
+   *
+   * The chosen direction and step length may overshoot or undershoot, and the
+   * function value might increase. We try to adapt the step length in order to
+   * ensure the function value decreases and step length is more or less
+   * optimal.
+   *
+   * @param function Objective function generating value and gradients from parameters
+   * @param observer Observer called with parameter updates
+   * @param step Suggested step in parameters, values and gradients
+   * @param direction Suggested step direction
+   *
+   * @return Number of function evaluations made in adjusting the step
+   */
+  template<
+    typename UpdateFunction,
+    typename Observer
+  > unsigned adjustStepAlongDirection(
+    UpdateFunction&& function,
+    Observer&& observer,
+    StepValues& step,
+    Eigen::Ref<VectorType> direction
+  ) {
+    unsigned iterations = 0;
+
+    for(iterations = 0; true; ++iterations) {
+      /* Armijo condition (Wolfe condition I) */
+      bool armijoCondition = (
+        step.values.current
+        <= step.values.old + c1 * stepLength * step.gradients.old.dot(direction)
+      );
+
+      /* Curvature condition (Wolfe condition II) */
+      bool curvatureCondition = (
+        step.gradients.current.dot(direction)
+        >= c2 * step.gradients.old.dot(direction)
+      );
+
+      /* Backtracking condition */
+      bool backtrack = (step.values.current > step.values.old);
+
+      // Decide whether to shorten or extend the step length
+      constexpr FloatType shortenFactor = 0.5;
+      constexpr FloatType lengthenFactor = 1.5;
+      static_assert(
+        std::fabs(shortenFactor * lengthenFactor - 1.0) > 0.1,
+        "Shortening and lengthening factor should not multiply to give "
+        "approximately one, this can cause oscillation"
+      );
+
+      if(!armijoCondition) {
+        stepLength *= shortenFactor;
+      } else if(!curvatureCondition) {
+        stepLength *= lengthenFactor;
+      }
+
+      /* If we don't have to backtrack, then don't re-evaluate along this
+       * direction, but accept the step and perform the next evaluation at
+       * the next position. We have improved the function value with the step
+       * and possibly adjusted the step length using the Wolfe conditions,
+       * that's enough.
+       */
+      if(backtrack && (!armijoCondition || !curvatureCondition)) {
+        // Retry shortened/lengthened step (no propagation needed)
+        step.parameters.current.noalias() = step.parameters.old + stepLength * direction;
+        /* Update gradients and value */
+        function(step.parameters.current, step.values.current, step.gradients.current);
+        observer(step.parameters.current);
+      } else {
+        break;
+      }
+    }
+
+    return iterations;
+  }
+
   template<
     typename UpdateFunction,
     typename Checker,
@@ -80,119 +216,46 @@ public:
     Observer&& observer = Observer {}
   ) {
     /* number of initialParameters treated */
-    const unsigned nParams = initialParameters.size();
-
-    detail::EigenUpdateBuffer<VectorType> parameters;
-    parameters.old = std::move(initialParameters);
-    detail::FloatUpdateBuffer<FloatType> values;
-    detail::EigenUpdateBuffer<VectorType> gradients;
-    gradients.old.resize(nParams);
-    gradients.current.resize(nParams);
-    CollectiveRingBuffer ringBuffer {nParams};
-
-    /* Steepest descent step: */
-    // Calculate initial function value and gradients
-    function(parameters.old, values.old, gradients.old);
-    observer(parameters.old);
-    // Generate direction
-    VectorType stepVector = FloatType {-0.1} * stepLength * gradients.old;
-    // Create new parameters
-    parameters.current.noalias() = parameters.old + stepLength * stepVector;
-    // Re-evaluate the function, assigning current value and gradient
-    function(parameters.current, values.current, gradients.current);
-    observer(parameters.current);
+    // Note: Do not reorder, StepValues moves from initialParameters
+    const unsigned P = initialParameters.size();
+    StepValues step;
+    VectorType direction = step.generateInitialDirection(function, initialParameters, stepLength);
+    CollectiveRingBuffer ringBuffer {P};
+    observer(step.parameters.current);
 
     unsigned iteration;
     for(
       iteration = 1;
       (
         !check.checkMaxIterations(iteration)
-        && !check.checkConvergence(parameters.current, values.current, gradients.current)
+        && !check.checkConvergence(step.parameters.current, step.values.current, step.gradients.current)
       );
       ++iteration
     ) {
-      /* The chosen direction and step length may overshoot or undershoot,
-       * and the function value might increase. We try to adapt the
-       * step length in order to ensure the function value decreases and
-       * step length is more or less optimal.
+      iteration += adjustStepAlongDirection(function, observer, step, direction);
+
+      ringBuffer.updateAndGenerateNewDirection(direction, step);
+
+      /* Accept the current step and store a new prospective step using the new
+       * direction vector
        */
-      if(linesearch) {
-        /* Armijo condition (Wolfe condition I) */
-        bool armijoCondition = (
-          values.current
-          <= values.old + c1 * stepLength * gradients.old.dot(stepVector)
-        );
-
-        /* Curvature condition (Wolfe condition II) */
-        bool curvatureCondition = (
-          gradients.current.dot(stepVector)
-          >= c2 * gradients.old.dot(stepVector)
-        );
-
-        /* Backtracking condition */
-        bool backtrack = (values.current > values.old);
-
-        // Decide whether to shorten or extend the step length
-        constexpr FloatType shortenFactor = 0.5;
-        constexpr FloatType lengthenFactor = 1.5;
-        static_assert(
-          std::fabs(shortenFactor * lengthenFactor - 1.0) > 0.1,
-          "Shortening and lengthening factor should not multiply to give "
-          "approximately one, this can cause oscillation"
-        );
-        if(!armijoCondition) {
-          stepLength *= shortenFactor;
-        } else if(!curvatureCondition) {
-          stepLength *= lengthenFactor;
-        }
-
-        /* Backtrack if we have to, otherwise accept the unoptimal, but
-         * still value-improving, step
-         */
-        if(backtrack && (!armijoCondition || !curvatureCondition)) {
-          // Retry shortened/lengthened step (no propagation needed)
-          parameters.current.noalias() = parameters.old + stepLength * stepVector;
-          /* Update gradients and value */
-          function(parameters.current, values.current, gradients.current);
-          observer(parameters.current);
-          continue;
-        }
-      }
-
-      /* Add a new element to the ring buffer */
-      ringBuffer.addNew(parameters, gradients);
-
-      // L-BFGS update: Use old parameters and gradients to improve direction
-      stepVector = -gradients.current;
-      ringBuffer.updateStepVector(stepVector);
-
-      /* Generate new parameters and get new value and gradient */
-      parameters.propagate();
-      values.propagate();
-      gradients.propagate();
-      parameters.current.noalias() = parameters.old + stepLength * stepVector;
-      function(parameters.current, values.current, gradients.current);
-      observer(parameters.current);
+      step.propagate(function, stepLength, direction);
+      observer(step.parameters.current);
     }
 
     // Copy the optimal parameters back into the in/out argument
-    initialParameters = std::move(parameters.current);
+    initialParameters = std::move(step.parameters.current);
     return iteration;
   }
 
-  /// @brief Switch to turn on and off the use of a linesearch
-  bool linesearch = true;
   /**
    * @brief 1st parameter for the Wolfe conditions.
-   *
-   * This parameter is only relevant if the linesearch is turned on.
    */
   FloatType c1 = 0.0001;
   /**
    * @brief 2nd parameter for the Wolfe conditions.
    *
-   * This parameter is only relevant if the linesearch is turned on.
-   * Also a value as low as 0.1 can be used.
+   * A value as low as 0.1 can be used.
    */
   FloatType c2 = 0.9;
   /**
@@ -215,7 +278,7 @@ private:
         dx(nParams, ringBufferSize)
     {}
 
-    void addNew(
+    void addInformation(
       const detail::EigenUpdateBuffer<VectorType>& parameterBuffer,
       const detail::EigenUpdateBuffer<VectorType>& gradientBuffer
     ) {
@@ -234,17 +297,17 @@ private:
       }
     }
 
-    void updateStepVector(Eigen::Ref<VectorType> stepVector) {
+    void improveDirection(Eigen::Ref<VectorType> direction) {
       VectorType alpha(count);
       auto newestToOldest = [&](unsigned i) {
         FloatType dxDotdg = dxDotDg(i);
         if(std::fabs(dxDotdg) < FloatType {1.0e-6}) {
-          alpha[i] = dx.col(i).dot(stepVector) / FloatType {1.0e-6};
+          alpha[i] = dx.col(i).dot(direction) / FloatType {1.0e-6};
         } else {
-          alpha[i] = dx.col(i).dot(stepVector) / dxDotdg;
+          alpha[i] = dx.col(i).dot(direction) / dxDotdg;
         }
 
-        stepVector.noalias() -= alpha[i] * dg.col(i);
+        direction.noalias() -= alpha[i] * dg.col(i);
       };
 
       // Newest to oldest
@@ -260,11 +323,11 @@ private:
         newestToOldest(i);
       }
 
-      stepVector *= dxDotDg(newestOffset) / dg.col(newestOffset).squaredNorm();
+      direction *= dxDotDg(newestOffset) / dg.col(newestOffset).squaredNorm();
 
       auto oldestToNewest = [&](unsigned i) {
         FloatType dxDotdg = dxDotDg(i);
-        FloatType beta = dg.col(i).dot(stepVector);
+        FloatType beta = dg.col(i).dot(direction);
 
         if(std::fabs(dxDotdg) < FloatType {1.0e-6}) {
           beta /= FloatType {1.0e-6};
@@ -272,7 +335,7 @@ private:
           beta /= dxDotDg(i);
         }
 
-        stepVector.noalias() += (alpha[i] - beta) * dx.col(i);
+        direction.noalias() += (alpha[i] - beta) * dx.col(i);
       };
 
       // Oldest to newest
@@ -283,6 +346,18 @@ private:
       for(unsigned i = 0; i < oldestOffset; ++i) {
         oldestToNewest(i);
       }
+    }
+
+    void updateAndGenerateNewDirection(
+      Eigen::Ref<VectorType> direction,
+      const StepValues& step
+    ) {
+      /* L-BFGS update: Use old parameters and gradients to improve steepest
+       * descent direction
+       */
+      addInformation(step.parameters, step.gradients);
+      direction = -step.gradients.current;
+      improveDirection(direction);
     }
   };
 };
