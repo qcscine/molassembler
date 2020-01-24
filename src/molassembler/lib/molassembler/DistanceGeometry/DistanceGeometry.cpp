18682d867c3cfccca90dec6df0db4aba4b3aa31a
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-01-24 09:35:23 +0100

    Fix missing license

--- src/molassembler/lib/molassembler/DistanceGeometry/DistanceGeometry.cpp
+++ src/molassembler/lib/molassembler/DistanceGeometry/DistanceGeometry.cpp
@@ -1,3 +1,9 @@
+/*!@file
+ * @copyright This code is licensed under the 3-clause BSD license.
+ *   Copyright ETH Zurich, Laboratory for Physical Chemistry, Reiher Group.
+ *   See LICENSE.txt
+ */
+
 #include "molassembler/DistanceGeometry/DistanceGeometry.h"
 
 namespace Scine {
