5b5e424aa59ae8c3a80a07d7380fea639ed3d193
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-03-12 07:46:06 +0100

    Add license to Loops.h

--- src/temple/lib/temple/Loops.h
+++ src/temple/lib/temple/Loops.h
@@ -1,3 +1,10 @@
+/*!@file
+ * @copyright This code is licensed under the 3-clause BSD license.
+ *   Copyright ETH Zurich, Laboratory for Physical Chemistry, Reiher Group.
+ *   See LICENSE.txt
+ * @brief Abstractions of variable-depth loops
+ */
+
 #ifndef INCLUDE_TEMPLE_LOOPS_H
 #define INCLUDE_TEMPLE_LOOPS_H
 
