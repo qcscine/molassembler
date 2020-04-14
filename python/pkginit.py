1aac35df918ed23151b8c1d78b690b9edc4d8632
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-04-14 09:36:36 +0200

    Refactor pkginit.py

--- python/pkginit.py
+++ python/pkginit.py
@@ -1,24 +1,13 @@
-def bootstrap():
-    # This is more or less copy-paste from importlib's docs
-    import importlib.util
-    import os
-    import sys
-    from pathlib import Path
-
-    path = os.path.join(
-        Path(__file__).parent.absolute(),
-        "scine_molassembler.so"
-    )
-    module_name = "scine_molassembler"
-
-    spec = importlib.util.spec_from_file_location(module_name, path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-
-    # Erase the function itself
-    global bootstrap
-    del bootstrap
-
-
-bootstrap()
+# This is more or less copy-paste from importlib's docs, makes the pybind11
+# module .so wild-importable (e.g. from `scine_molassembler.io import *`)
+import importlib.util
+import os
+import sys
+from pathlib import Path
+spec = importlib.util.spec_from_file_location(
+    __name__,
+    os.path.join(Path(__file__).parent.absolute(), __name__ + ".so")
+)
+module = importlib.util.module_from_spec(spec)
+sys.modules[__name__] = module
+spec.loader.exec_module(module)
