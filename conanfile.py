1fd72fa250edcd362eff07b121b668c77a6fcc37
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-07-03 15:22:28 +0200

    Adopt new conan naming scheme

--- conanfile.py
+++ conanfile.py
@@ -36,7 +36,7 @@ class MolassemblerConan(ScineConan):
         "python/*",
         "CMakeLists.txt",
     ]
-    requires = "scine_utilities/[~=3.0.0]@ci/develop"
+    requires = "scine_utilities/[~=3.0.0]@scine/develop"
 
     def _configure_cmake(self):
         return super()._configure_cmake_base("molassembler", None)
