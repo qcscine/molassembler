31ae7657a905aa4a2afc322a61a3736a965ce9b0
Jan-Grimo Sobez <jan-grimo.sobez@phys.chem.ethz.ch>
2020-10-08 14:25:34 +0200

    Remove old raytracing, add CMake-only CI build

--- analysis/raytrace/raytrace_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for filename in ./*.pov; do
-  `povray -I$filename -D0 -H1440 -W2560`
-done
--- analysis/raytrace/single_case/raytrace_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for filename in ./*.pov; do
-  `povray -I$filename -D0 -H768 -W1366`
-done
